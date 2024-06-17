463e70d447ae29763771b407236338f3a595a076
Lara <laralauro@outlook.com>
2024-06-17 10:34:31 +0200

    Table created

--- mastermind.js
+++ mastermind.js
@@ -7,6 +7,51 @@ const SELECT_GREEN = document.getElementById("green");
 const SELECT_PINK = document.getElementById("pink");
 const SELECT_PURPLE = document.getElementById("purple");
 
-const GAME_CONTAINER = document.getElementById("gameContainer");
+const GAME_TABLE = document.getElementById("gameTable");
 
-const GAME_ARRAY = [];
+function gameTableCreate() {
+  for (let i = 0; i < 12; i++) {
+    const TR = document.createElement("tr");
+
+    GAME_TABLE.appendChild(TR);
+    TR.classList.add(
+      "w-full",
+      "h-full",
+      "flex",
+      "justify-evenly",
+      "items-center",
+      "py-1"
+    );
+
+    for (let index = 0; index < 5; index++) {
+      const TD = document.createElement("td");
+      TR.append(TD);
+      TD.classList.add("rounded-full", "h-8", "w-8", "border-2", "mx-1");
+    }
+
+    for (let dex = 0; dex < 4; dex++) {
+      const DIV = document.createElement("div");
+      TR.lastChild.append(DIV);
+
+      TR.lastChild.classList.remove(
+        "rounded-full",
+        "h-8",
+        "w-8",
+        "border-2",
+        "mx-1"
+      );
+      TR.lastChild.classList.add(
+        "rounded-xl,",
+        "h-8",
+        "w-20",
+        "border-2",
+        "flex",
+        "justify-evenly",
+        "items-center"
+      );
+      DIV.classList.add("w-4", "h-4", "rounded-full", "border");
+    }
+  }
+}
+
+gameTableCreate();
